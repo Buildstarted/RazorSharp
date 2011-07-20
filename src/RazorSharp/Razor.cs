630c61daaea1f54590f1dbb2ec4cf7f62e1539e9
Ben <ben@dornis.org>
2011-07-20 10:39:46 -0700

    Basic RazorSharp implementation

---
+++ src/RazorSharp/Razor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RazorSharp {
+    public class Razor {
+        public static string Parse(string template) {
+            return RazorCompiler.Render(template).Result;
+        }
+
+        public static string Parse(string template, string name) {
+            return RazorCompiler.Render(template, name).Result;
+        }
+
+        public static string Parse(string template, string name, string masterTemplate) {
+            return RazorCompiler.Render(template, name, masterTemplate).Result;
+        }
+
+        public static string Parse<T>(T model, string template) {
+            return RazorCompiler.Render<T>(model, template).Result;
+        }
+
+        public static string Parse<T>(T model, string template, string name) {
+            return RazorCompiler.Render<T>(model, template, name).Result;
+        }
+
+        public static string Parse<T>(T model, string template, string name, string masterTemplate) {
+            return RazorCompiler.Render<T>(model, template, name, masterTemplate).Result;
+        }
+
+    }
+}
---
+++ src/RazorSharp/RazorHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RazorSharp {
+    public class RazorHelper {
+
+        ITemplateBase template;
+
+        public string Name {
+            get {
+                return template.Name;
+            }
+        }
+
+        public RazorHelper(ITemplateBase templateBase) {
+            this.template = templateBase;
+        }
+    }
+}
