1f16c3d51a59a645f2b146f3f771d6fe377de26e
Roy Haddad <roy@1099pro.com>
2014-04-08 08:03:47 -0700

    Convert tests to NUnit, and make style changes

--- src/RazorSharp.Tests/Tests.cs
+++ src/RazorSharp.Tests/Tests.cs
@@ -1,62 +1,65 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿namespace RazorSharp.Tests
+{
+    using NUnit.Framework;
 
-namespace RazorSharp.Tests {
-    [TestClass]
-    public class Tests {
-        string Name = "Some User";
+    [TestFixture]
+    public class Tests
+    {
+        const string UserName = "Some User";
 
-        [TestMethod]
-        public void razor_parser_with_anonymouse_type() {
-            string razor = "@Model.Name";
+        [Test]
+        public void razor_parser_with_anonymouse_type()
+        {
+            const string template = "@Model.Name";
 
-            string result = RazorSharp.Razor.Parse(new { Name = Name }, razor);
-            Assert.AreEqual(Name, result);
+            var result = Razor.Parse(new { Name = UserName }, template);
+            Assert.AreEqual(UserName, result);
         }
 
-        [TestMethod]
-        public void razor_parser_with_concrete_type() {
-            string razor = "@Model.Name";
+        [Test]
+        public void razor_parser_with_concrete_type()
+        {
+            const string template = "@Model.Name";
 
-            string result = RazorSharp.Razor.Parse(new User { Name = Name }, razor);
-            Assert.AreEqual(Name, result);
+            var result = Razor.Parse(new User { Name = UserName }, template);
+            Assert.AreEqual(UserName, result);
         }
 
-        [TestMethod]
-        public void razor_parser_with_master_template() {
-            string razor = "@Model.Name";
-            string master = "Master Page\r\n@RenderBody()";
+        [Test]
+        public void razor_parser_with_master_template()
+        {
+            const string template = "@Model.Name";
+            const string masterTemplate = "Master Page\r\n@RenderBody()";
 
-            string result = RazorSharp.Razor.Parse(new { Name = Name }, razor, master);
-            Assert.IsTrue(result.Contains(Name));
+            var result = Razor.Parse(new { Name = UserName }, template, masterTemplate);
+            Assert.IsTrue(result.Contains(UserName));
             Assert.IsTrue(result.Contains("Master Page"));
         }
 
-        [TestMethod]
-        public void razor_parser_with_name_results_in_subsequent_calls_from_cache() {
-            string razor = "@Model.Name";
-            string cacheName = "username";
+        [Test]
+        public void razor_parser_with_name_results_in_subsequent_calls_from_cache()
+        {
+            const string template = "@Model.Name";
+            const string cacheName = "username";
 
-            string result = RazorSharp.Razor.Parse(new { Name = Name }, razor, cacheName, null);
-            Assert.AreEqual(Name, result);
-            var instance = RazorSharp.Razor.compiler.Render(new { Name = Name }, razor, cacheName, null); 
+            var result = Razor.Parse(new { Name = UserName }, template, cacheName, null);
+            Assert.AreEqual(UserName, result);
+            var instance = Razor.compiler.Render(new { Name = UserName }, template, cacheName, null); 
             Assert.AreEqual(instance.Cached, true);
         }
 
-        [TestMethod]
-        public void razor_parser_with_mastertemplate_and_name_results_in_subsequent_calls_from_cache() {
-            string razor = "@Model.Name";
-            string master = "Master Page\r\n@RenderBody()";
-            string cacheName = "username_master";
+        [Test]
+        public void razor_parser_with_mastertemplate_and_name_results_in_subsequent_calls_from_cache()
+        {
+            const string template = "@Model.Name";
+            const string masterTemplate = "Master Page\r\n@RenderBody()";
+            const string cacheName = "username_master";
 
-            string result = RazorSharp.Razor.Parse(new { Name = Name }, razor, cacheName, master);
-            Assert.IsTrue(result.Contains(Name));
+            var result = Razor.Parse(new { Name = UserName }, template, cacheName, masterTemplate);
+            Assert.IsTrue(result.Contains(UserName));
             Assert.IsTrue(result.Contains("Master Page"));
 
-            var instance = RazorSharp.Razor.compiler.Render(new { Name = Name }, razor, cacheName, master);
+            var instance = Razor.compiler.Render(new { Name = UserName }, template, cacheName, masterTemplate);
             Assert.AreEqual(true, instance.Cached);
             Assert.AreEqual(cacheName + "_masterTemplate", instance.Name);
         }
